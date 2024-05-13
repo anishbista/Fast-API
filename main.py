ab1022dbe9a42a04a60addef0a70ef709bb49a84
anishbista92 <anishbista9235@gmail.com>
2024-05-13 13:11:06 +0545

    Path parameters

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", description="This is just practice", deprecated=False)
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Worldsasd"}
+
+
+@app.post("/")
+async def post():
+    return {"message": "Hello from the post "}
+
+
+@app.put("/put")
+async def put():
+    return {"message": "This is put method"}
+
+
+@app.get("/items/me")
+async def get_items():
+    return {"message": "This is test"}
+
+
+@app.get("/items/{id}")
+async def list_items(id: str):
+    print("dadadads", type(id))
+    if not isinstance(id, str):
+        return {"message": "please enter"}
+    else:
+        return {"id": id}
+
+
+class FoodEnum(str, Enum):
+    vegatables = "cauli"
+    fruits = "apple"
+
+
+@app.get("/food_items/{name}")
+async def food_items(name: FoodEnum):
+    if name == FoodEnum.vegatables:
+        return {"message": "it is vegatbles"}
+    else:
+        return {"message": "It is fruits"}
