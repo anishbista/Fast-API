657bcf32b278c7d459df2e1b923373e29575473f
anishbista92 <anishbista9235@gmail.com>
2024-05-18 09:30:04 +0545

    Nested Models

--- main1.py
+++ main1.py
@@ -11,9 +11,33 @@ class Item(BaseModel):
     description: str | None = Field(
         ..., title="It is description of descriptoin", max_length=5
     )
+    tags: list[int]
+    # tags: set = set()
 
 
 @app.post("/items/{item_id}")
 async def post_items(item_id: int, item: Item = Body(..., embed=True)):
     results = {"itemd_id": item_id, "item": item}
+    return results
+
+
+# Part 9 -> Body-Nested Models
+
+
+class Image(BaseModel):
+    name: str
+    items: list[Item]
+
+
+@app.post("/product/{item_id}")
+async def post_items(item_id: int, image: list[Image] = Body(..., embed=True)):
+    results = {"itemd_id": item_id, "image": image}
+    return results
+
+
+@app.post("/test")
+async def post_items(
+    test: dict[str, str]
+):  # ict[str, str] this expects key as str and value also as int
+    results = {"itemd_id": test}
     return results
